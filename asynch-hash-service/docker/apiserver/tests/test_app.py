a3a459dc2f64038ab58d2b5a1ba6bd84cfbcf3f6
ZanNen <github3746@ashleygc.co.uk>
2025-02-04 09:18:03 +0000

    Use better fixture (#11)

--- asynch-hash-service/docker/apiserver/tests/test_app.py
+++ asynch-hash-service/docker/apiserver/tests/test_app.py
@@ -9,50 +9,52 @@ def test_root_page(client):
     assert j["status"] == "OK"
 
 
-def test_token(flask_app):
+def test_token(client):
     """
     Test token issuing and verifying
     """
 
-    with flask_app.test_client() as test_client:
-        response = test_client.post("/token", json={})
-        assert response.status_code == 200, response.data
-        j = response.json
-        assert j["error"] is None
-        token = j["token"]
+    response = client.post("/token", json={})
+    assert response.status_code == 200, response.data
+    j = response.json
+    assert j["error"] is None
+    token = j["token"]
 
-        response = test_client.post(
-            "/token/verify",
-            headers={"API-Key": token},
-        )
-        assert response.status_code == 200, response.data
-        j = response.json
-        assert j["error"] is None, response.data
+    response = client.post(
+        "/token/verify",
+        headers={"API-Key": token},
+    )
+    assert response.status_code == 200, response.data
+    j = response.json
+    assert j["error"] is None, response.data
 
 
-def test_job_create_get(flask_app):
-    with flask_app.test_client() as test_client:
-        response = test_client.post("/token", json={})
-        assert response.status_code == 200, response.data
-        j = response.json
-        assert j["error"] is None
-        token = j["token"]
+def test_job_create_get(client):
+    """
+    Test job creation and fetching
+    """
 
-        response = test_client.post(
-            "/jobs",
-            headers={"API-Key": token},
-            json={},
-        )
-        assert response.status_code == 200, response.data
-        j = response.json
-        assert j["error"] is None
-        job_id = j["job"]["id"]
+    response = client.post("/token", json={})
+    assert response.status_code == 200, response.data
+    j = response.json
+    assert j["error"] is None
+    token = j["token"]
 
-        response = test_client.get(
-            f"/jobs/{job_id}",
-            headers={"API-Key": token},
-            json={},
-        )
-        assert response.status_code == 200, response.data
-        j = response.json
-        assert j["error"] is None
+    response = client.post(
+        "/jobs",
+        headers={"API-Key": token},
+        json={},
+    )
+    assert response.status_code == 200, response.data
+    j = response.json
+    assert j["error"] is None
+    job_id = j["job"]["id"]
+
+    response = client.get(
+        f"/jobs/{job_id}",
+        headers={"API-Key": token},
+        json={},
+    )
+    assert response.status_code == 200, response.data
+    j = response.json
+    assert j["error"] is None
